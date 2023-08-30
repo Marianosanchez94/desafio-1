4252fbc2ba1b32ba4c366ad09bbe1fbb52f45de3
Mariano Sanchez <mnicolas.sanchez94@gmail.com>
2023-08-29 23:30:20 -0300

    desafio 2

--- index.js
+++ index.js
@@ -1,63 +1,66 @@
-// title (nombre del producto)
-// description (descripción del producto)
-// price (precio)
-// thumbnail (ruta de imagen)
-// code (código identificador)
-// stock (número de piezas disponibles)
+    const fs = require('fs');
 
-class ProductManager {
-    constructor(){
-        this.products = [];
-        this.nextId = 1;
+    class ProductManager {
+    constructor(filePath) {
+        this.filePathpath = filePath;
     }
-    addProduct(title,description,price,thumbnail,code,stock){
-        let newId;
-        if(this.products.length==0){
-            newId = 1;
-        }else{
-            newId = this.products[this.products.length-1].id+1
-        }
 
-        const newProduct = {
-            id: newId,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
+    readData() {
+        try {
+        const data = fs.readFileSync(this.filepath, 'utf8');
+        return JSON.parse(data);
+        } catch (error) {
+        throw new error("No puede realizarse la operacion");
         }
-        this.products.push(newProduct);
+    }
+
+    writeData(data) {
+        fs.writeFileSync(this.path, JSON.stringify(data, null, 2));
+    }
+
+    addProduct(product) {
+        const products = this.readData();
+        const id = products.length > 0 ? products[products.length - 1].id + 1 : 1;
+        const newProduct = { ...product, id };
+        products.push(newProduct);
+        this.writeData(products);
+    }
+
+    getProducts() {
+        return this.readData();
+    }
+
+    getProductById(id) {
+        const products = this.readData();
+        return products.find(product => product.id === id);
+    }
+
+    updateProduct(id, updatedFields) {
+        const products = this.readData();
+        const index = products.findIndex(product => product.id === id);
         
-        //Validar campos
-        if(!title || !description || !price || !thumbnail || !code || !stock ){
-            console.log("Todos los campos son obligatorios")
-        }
-        //Validar code
-        let newCode;
-        if (newCode === this.products.code){
-            console.log("Ya existe este codigo")
-        }
-
-    }
-
-    getProducts(){
-        console.log(this.products);
-    }
-
-    getProductById(){
-        const productId = this.products.find(product => product.id === id)
-        if (!productId){
-            console.error("Not Found");
-
+        if (index !== -1) {
+        products[index] = { ...products[index], ...updatedFields };
+        this.writeData(products);
         }
     }
 
-}
+    deleteProduct(id) {
+        const products = this.readData();
+        const updatedProducts = products.filter(product => product.id !== id);
+        this.writeData(updatedProducts);
+    }
+    }
 
-const manager1 = new ProductManager();
-manager1.addProduct("Iphone","Celular","$1000","url", 10 , 19 );
-manager1.addProduct("Iphone","Celular","$1000","url", 10 , 19 );
+    const productManager = new ProductManager('products.json');
 
+    productManager.addProduct({ Title: 'Producto 1',Description:'desc', price: 5000,thumbnail : 'url', code : 4 ,stock: 20});
+    productManager.addProduct({ Title: 'Producto 2',Description:'desc', rice: 1000, thumbnail : 'url', code : 2, stock: 12});
+    console.log(productManager.getProducts());
 
-manager1.getProducts();
+    const productToUpdate = productManager.getProductById(1);
+    if (productToUpdate) {
+    productManager.updateProduct(1, { price: 15.99 });
+    }
+
+    productManager.deleteProduct(2);
